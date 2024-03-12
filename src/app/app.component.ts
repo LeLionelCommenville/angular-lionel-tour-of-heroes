ddb21371b9a7f77150eace25a161504f4308fe35
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-12 16:06:20 +0100

    migrate the module style angular application to standalone angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone: true,
+    imports: [HeroesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { NgFor } from '@angular/common';
+import { NgFor, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrl: './heroes.component.css',
+    standalone: true,
+    imports: [FormsModule, UpperCasePipe]
 })
 export class HeroesComponent {
   heroes = HEROES;
