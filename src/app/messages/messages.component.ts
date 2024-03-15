fb5ab9c806c2d8ba69d6eaf71f57545146d8c6cb
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-15 18:37:08 +0100

    create hero service and message services and component

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css',
+  standalone: true,
+  imports: [NgFor, NgIf],
+})
+export class MessagesComponent {
+  constructor(public messageService: MessageService) {}
+}
